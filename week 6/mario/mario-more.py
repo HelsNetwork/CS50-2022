2f819e8544ea9e238bd0a0481a9dea11bf6baa86
hel- <87879515+hel4s@users.noreply.github.com>
2022-04-11 22:57:25 +0200

    Create mario-more.py

---
+++ week 6/mario/mario-more.py
@@ -0,0 +1,11 @@
+# TODO
+from cs50 import get_int
+
+height = 0
+while (height > 8 or height < 1):
+    height = get_int("Height:\n")
+
+for i in range(1, height + 1):
+    print(" " * (height - i), end="")
+    print("#" * i, end="  ")
+    print("#" * i)
