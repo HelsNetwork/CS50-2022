78916ee73c4173075974acbdaf85e2ba55e16628
hel- <87879515+hel4s@users.noreply.github.com>
2022-04-12 21:49:56 +0200

    Create 11.sql

---
+++ week 7/movies/11.sql
@@ -0,0 +1,1 @@
+SELECT movies.title from people JOIN stars ON people.id = stars.person_id JOIN movies ON stars.movie_id = movies.id JOIN ratings on movies.id = ratings.movie_id WHERE people.name = "Chadwick Boseman" ORDER BY rating DESC LIMIT 5;
